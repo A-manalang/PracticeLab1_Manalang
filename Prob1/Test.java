d3c78b6f9783318f6189a91233343cc702520603
A-manalang <70572535+A-manalang@users.noreply.github.com>
2020-09-01 17:34:15 +0800

    Create Test.java

---
+++ Prob1/Test.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> nNum = new ArrayList<Integer>();
+		
+		nNum.add(1);
+		nNum.add(33);
+		nNum.add(3);
+		nNum.add(4);
+		
+		
+
+		System.out.print(" = " + IntegerClass.Add(nNum));
+		}
+
+	}
+
+
